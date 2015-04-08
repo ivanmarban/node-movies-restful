11cc12c1ffd385847324e17f2768da8585d74cd6
Ivan Marban <ivan.marban@gmail.com>
2015-04-08 14:23:52 +0200

    Movie schema transformation

--- models/movie.js
+++ models/movie.js
@@ -9,4 +9,11 @@ var MovieSchema = new mongoose.Schema({
 	director : { type : String }
 });
 
+MovieSchema.set('toJSON', {
+	transform: function (doc, ret, options) {
+		ret.id = ret._id;
+		delete ret._id;
+	}
+});
+
 module.exports = mongoose.model('Movie', MovieSchema);
